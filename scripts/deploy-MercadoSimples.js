bce6f2c488367df85c488cfad2a08fd48bc87fe0
Joao Pedro <joaoaffonso@id.uff.br>
2025-02-18 18:29:18 -0300

    Adicionando previsões e votos no script de deploy

--- scripts/deploy-MercadoSimples.js
+++ scripts/deploy-MercadoSimples.js
@@ -1,6 +1,10 @@
 // scripts/deploy-banner.js
 const { ethers } = require("hardhat");
 
+async function getRandomBetween(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
 async function main() {
   //criando listas de usuários
   const lista_de_eleitores = await ethers.getSigners();
@@ -23,6 +27,31 @@ async function main() {
   //mintando tokens para os endereços
   for (let i = 0; i <= 19; i++) {
     await token.mint(lista_de_eleitores[i],10000);
+  }
+
+  // Cadastrando previsões
+  await mercado.addPrevisao("Previsao 1", "Descrição previsão 1");
+  await mercado.addPrevisao("Previsao 2", "Descrição previsão 2");
+  await mercado.addPrevisao("Previsao 3", "Descrição previsão 3");
+
+  // lançando votos
+  let temp;
+  for (let id = 1; id <= 3; id++){
+    for (let i = 1; i <= 10; i++) {
+      temp = await getRandomBetween(1,10);
+      temp = 100*temp;
+      await token.connect(lista_de_eleitores[i]).approve(mercadoAddress,temp);
+      await mercado.connect(lista_de_eleitores[i]).votar(id,temp);
+      console.log("Eleitor ", i, " votou ", temp, " na predição", id);
+    }
+    for (let i = 11; i <= 18; i++) {
+      temp = await getRandomBetween(1,10);
+      temp = 100*temp;
+      await token.connect(lista_de_eleitores[i]).approve(mercadoAddress,temp);
+      temp = -temp;
+      await mercado.connect(lista_de_eleitores[i]).votar(id,temp);
+      console.log("Eleitor ", i, " votou ", temp, " na predição", id);
+    }
   }
 }
 
